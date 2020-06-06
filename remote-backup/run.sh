abaa25dab2922b2b7bb6aa6f3251fe796d2bf241
Krzysztof Kowalczyk <krz.kowalczyk@gmail.com>
2020-06-06 21:22:25 +0200

    fix: fix cron entry

--- remote-backup/run.sh
+++ remote-backup/run.sh
@@ -8,7 +8,10 @@ CRON_SCHEDULE=$(jq --raw-output ".schedule" $CONFIG_PATH)
 
 touch /hassio-backup.log
 echo "Configuring cron"
-echo "${CRON_SCHEDULE} sh -c /backup.sh >> /hassio-backup.log 2>&1"
+echo "${CRON_SCHEDULE} sh -c /backup.sh >> /hassio-backup.log 2>&1" >> /etc/crontabs/root
+
+echo "Current cron config:"
+cat /etc/crontabs/root
 
 echo "Starting cron"
 crond && tail -f /hassio-backup.log
