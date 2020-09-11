79f27f28d9bf2ad5913292479236dcf4ef2fe73b
Krzysztof Kowalczyk <krz.kowalczyk@gmail.com>
2020-09-11 21:24:50 +0200

    feat: add snapshot encryption

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog][keep-a-changelog] this project adheres to [Semantic Versioning][semantic-versioning].
 
+## [v0.2.0] (2020-09-11)
+### Changed
+
+- Encryption of snapshots is now done by build-in snapshot functionality, instead of password protected archive
+
 ## [v0.1.0] (2020-06-08)
 
 ### Added
--- remote-backup2/backup.sh
+++ remote-backup2/backup.sh
@@ -9,7 +9,7 @@ SSH_PORT=$(jq --raw-output ".ssh.port" $CONFIG_PATH)
 SSH_USER=$(jq --raw-output ".ssh.user" $CONFIG_PATH)
 SSH_KEY=$(jq --raw-output ".ssh.key[]" $CONFIG_PATH)
 REMOTE_DIRECTORY=$(jq --raw-output ".remote_directory" $CONFIG_PATH)
-ZIP_PASSWORD=$(jq --raw-output '.zip_password' $CONFIG_PATH)
+SNAPSHOT_PASSWORD=$(jq --raw-output '.snapshot_password' $CONFIG_PATH)
 KEEP_LOCAL_BACKUP=$(jq --raw-output '.keep_local_backup' $CONFIG_PATH)
 
 SMB_HOST=$(jq --raw-output ".smb.host" $CONFIG_PATH)
@@ -39,28 +39,16 @@ function add-ssh-key {
 function copy-backup-to-remote-ssh {
 
     cd /backup/
-    if [[ -z $ZIP_PASSWORD  ]]; then
-      echo "Copying ${slug}.tar to ${REMOTE_DIRECTORY} on ${SSH_HOST} using SCP"
-      scp -F "${HOME}/.ssh/config" "${slug}.tar" remote:"${REMOTE_DIRECTORY}"
-    else
-      echo "Copying password-protected ${slug}.zip to ${REMOTE_DIRECTORY} on ${SSH_HOST} using SCP"
-      zip -P "$ZIP_PASSWORD" "${slug}.zip" "${slug}".tar
-      scp -F "${HOME}/.ssh/config" "${slug}.zip" remote:"${REMOTE_DIRECTORY}" && rm "${slug}.zip"
-    fi
+    echo "Copying ${slug}.tar to ${REMOTE_DIRECTORY} on ${SSH_HOST} using SCP"
+    scp -F "${HOME}/.ssh/config" "${slug}.tar" remote:"${REMOTE_DIRECTORY}"
 
 }
 
 function copy-backup-to-remote-smb {
 
     cd /backup/
-    if [[ -z $ZIP_PASSWORD  ]]; then
-      echo "Copying ${slug}.tar to smb://${SMB_HOST}/${REMOTE_DIRECTORY} using curl"
-      curl --upload-file "${slug}.tar" -u "${SMB_USER}:${SMB_PASSWORD}" "smb://${SMB_HOST}/${REMOTE_DIRECTORY}"
-    else
-      echo "Copying password-protected ${slug}.zip to ${REMOTE_DIRECTORY} on ${SMB_HOST} using curl"
-      zip -P "$ZIP_PASSWORD" "${slug}.zip" "${slug}".tar
-      curl --upload-file "${slug}.zip" -u "${SMB_USER}:${SMB_PASSWORD}" "smb://${SMB_HOST}/${REMOTE_DIRECTORY}" && rm "${slug}.zip"
-    fi
+    echo "Copying ${slug}.tar to smb://${SMB_HOST}/${REMOTE_DIRECTORY} using curl"
+    curl --upload-file "${slug}.tar" -u "${SMB_USER}:${SMB_PASSWORD}" "smb://${SMB_HOST}/${REMOTE_DIRECTORY}"
 
 }
 
@@ -78,7 +66,7 @@ function delete-local-backup {
         last_date_to_keep=$(hassio snapshots list --raw-json | jq .data.snapshots[].date | sort -r | \
             head -n "${KEEP_LOCAL_BACKUP}" | tail -n 1 | xargs date -D "%Y-%m-%dT%T" +%s --date )
 
-        hassio snapshots list --raw-json | jq -c .data.snapshots[] | while read backup; do
+        hassio snapshots list --raw-json | jq -c .data.snapshots[] | while read -r backup; do
             if [[ $(echo ${backup} | jq .date | xargs date -D "%Y-%m-%dT%T" +%s --date ) -lt ${last_date_to_keep} ]]; then
                 echo "Deleting local backup: $(echo ${backup} | jq -r .slug)"
                 hassio snapshots remove "$(echo ${backup} | jq -r .slug)"
@@ -90,8 +78,13 @@ function delete-local-backup {
 
 function create-local-backup {
     name="Automated backup $(date +'%Y-%m-%d %H:%M')"
-    echo "Creating local backup: \"${name}\""
-    slug=$(hassio snapshots new --name="${name}" --raw-json | jq --raw-output '.data.slug')
+    if [[ -z $SNAPSHOT_PASSWORD  ]]; then
+        echo "Creating local backup: \"${name}\""
+        slug=$(hassio snapshots new --name="${name}" --raw-json | jq --raw-output '.data.slug')
+    else
+        echo "Creating local password protected backup: \"${name}\""
+        slug=$(hassio snapshots new --name="${name}" --password="${SNAPSHOT_PASSWORD}" --raw-json | jq --raw-output '.data.slug')
+    fi
     echo "Backup created: ${slug}"
 }
 
